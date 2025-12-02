2a8f8b8b67c168e34fccba2daf46e12b48e35b7a
Ramkumar S <ramkumarsrinivasan5@gmail.com>
2025-12-02 22:08:21 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/New-folder--3-/',   // IMPORTANT: use your actual GitHub repo name here
+  base: '/React-project/',   // IMPORTANT: use your actual GitHub repo name here
 })
